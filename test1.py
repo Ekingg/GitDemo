b92d2bc420b5418eb3b1db417f995e0de993cacb
Ekingg <ekin.wu@gmail.com>
2019-10-27 09:14:06 +0800

    4th commit

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
 cond = 1
-while cond<=10:
+while cond <= 10:
     print(cond)
-    cond=cond+1
+    cond = cond + 1
 
 import random
 import math
 
-#Demo Version2 test
+# Demo Version2 test
 
-#Demo Version3 test
+# Demo Version3 test
 
-#Demo Version4 test
+# Demo Version4 test
