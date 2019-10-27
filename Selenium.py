f786a1f3399371a1b8bf62e47f3bb2a187b5e194
Ekingg <ekin.wu@gmail.com>
2019-10-27 13:58:53 +0800

    6th commit

--- Selenium.py
+++ Selenium.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
-from chromedriver_py import binary_path # this will get you the path variable
-
+from chromedriver_py import binary_path  # this will get you the path variable
 driver = webdriver.Chrome(executable_path=binary_path)
-#driver.fullscreen_window()
-driver.get("http://www.python.org")
-driver.
+# driver.fullscreen_window()
+
+driver.get('http://www.google.com/');
+#time.sleep(5)  # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+#time.sleep(5)  # Let the user actually see something!
+driver.quit()
